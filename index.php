2aef1b9faccd231a3b9ea96dc0666b4bb55fc7a6
Nisansala Madushani <48275251+nishini@users.noreply.github.com>
2022-12-17 20:03:12 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<div class="form-area">
+  <form action="connection/login_fun.php" method="post">
+    <h1>ADMIN LOGIN</h1>
+   
+    <p>Email</p>
+    <input type="text" placeholder="Enter Email" name="user" >
+
+    <p>Password</p>
+    <input type="password" placeholder="Enter Password" name="password" >
+    
+     
+    <button type="submit" class=" btn"  name ="action" value="student"> Login</button>
+    
+  
+  </form>
+
+     
+</div>

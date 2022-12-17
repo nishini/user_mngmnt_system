239110c6e51991907b9524e3c36fd56963bf99e5
Nisansala Madushani <48275251+nishini@users.noreply.github.com>
2022-12-17 20:29:26 +0530

    add validations

--- connection/login_fun.php
+++ connection/login_fun.php
@@ -1,73 +1,29 @@
 <?php
-include("../db/config.php"); //add db connection
+include("../db/config.php");
 session_start();
 
-if(isset($_POST['name'])) {
-	$name = htmlspecialchars(strip_tags(trim($_POST['name'])), ENT_QUOTES, 'UTF-8'); 
-	$address  = htmlspecialchars(strip_tags(trim($_POST['address'])), ENT_QUOTES, 'UTF-8');
-	$nic  = htmlspecialchars(strip_tags(trim($_POST['nic'])), ENT_QUOTES, 'UTF-8');
-	$contact_no1  = htmlspecialchars(strip_tags(trim($_POST['tel_no'])), ENT_QUOTES, 'UTF-8');
-	$contact_no2  = htmlspecialchars(strip_tags(trim($_POST['contact_no2'])), ENT_QUOTES, 'UTF-8');
-	$u_name  = htmlspecialchars(strip_tags(trim($_POST['uname'])), ENT_QUOTES, 'UTF-8');
-	$password  = htmlspecialchars(strip_tags(trim($_POST['password'])), ENT_QUOTES, 'UTF-8');
-	$email  = htmlspecialchars(strip_tags(trim($_POST['email'])), ENT_QUOTES, 'UTF-8');
-	$pwd = hash('sha256', $password);
+$username = $_POST['user'];
+$password = $_POST['password'];
 
-	$folder_path = '../img/';
+$username = stripcslashes($username);
+$password = stripcslashes($password);
+$password = hash('sha256', $password); // password encrypt 
 
-	$filename = basename($_FILES['img']['name']);
-	$newname = $folder_path . $filename;
+$sql = "SELECT * from users where u_name = '$username'  and is_admin=1 " or die(mysql_error());
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count = mysqli_num_rows($result);
+$id = $row['u_name'];
+$pwd = $row['password'];
 
-	$FileType = pathinfo($newname,PATHINFO_EXTENSION);
+if ($count == 1 && $pwd==$password  ) {
+ // check user count and password
+    header("Location:../add_users.php");
+    $_SESSION['id'] =  $id;
+    $_SESSION['uname'] =  $id;
+} else if ($count == 1 && $password!=$row['password']) {
 
-
-	$sqlu = "SELECT * FROM users WHERE u_name='$u_name' OR email='$email' ";
-	$stmtu = mysqli_query($conn, $sqlu);
-	$countu = mysqli_num_rows($stmtu);
-
-
-	if($countu == 0){
-
-
-		if (move_uploaded_file($_FILES['img']['tmp_name'], $newname))
-		{
-
-			$filesql = "INSERT INTO users SET
-			name='$name',
-			address='$address',
-			nic='$nic',
-			contact_no1='$contact_no1',
-			contact_no2='$contact_no1',
-			u_name='$u_name',
-			password='$pwd',
-			email='$email',
-			is_admin = 0,
-			img='$filename'
-
-
-
-			";
-			$fileresult = mysqli_query($conn,$filesql);
-
-			if ($fileresult)
-			{
-                
-				header("location: ../add_users.php?status=1");
-			} else
-			{
-            // header("location: ../add_users.php?status=0");
-				echo $filesql;
-			}
-		}
-		else
-		{
-
-			header("location: ../add_users.php?status=3");
-		}
-
-	}else{
-		header("location: ../add_users.php?status=2");
-	}
+	 header("Location:../index.php?error=Invalid Password");
+} else {
+	 header("Location:../index.php?error=Invalid Login");
 }
-
-?>

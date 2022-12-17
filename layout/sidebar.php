c4749d4a202a3740b3fba78a666f1398ebb631e5
Nisansala Madushani <48275251+nishini@users.noreply.github.com>
2022-12-17 19:54:07 +0530

    initial commit

---
+++ layout/sidebar.php
@@ -0,0 +1,82 @@
+<ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="admin.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-book"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3"></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="admin.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item active">
+        <a class="nav-link collapsed" href="add_users.php">
+            <span>Add Users</span>
+        </a>
+    </li>
+
+    <!-- Nav Item - Utilities Collapse Menu -->
+    <li class="nav-item active">
+        <a class="nav-link collapsed" href="user_edit.php">
+            <span>Edit/Delete Users</span>
+        </a>
+    </li>
+<!-- 
+    <li class="nav-item active">
+        <a class="nav-link collapsed" href="add_books.php">
+            <span>Books</span>
+        </a>
+    </li> -->
+
+  <!--   <li class="nav-item active">
+        <a class="nav-link collapsed" href="add_doctors.php">
+            <span>Add Doctors</span>
+        </a>
+    </li> -->
+
+  <!--   <li class="nav-item active">
+        <a class="nav-link" href="orders.php">
+            <span>Orders</span>
+        </a>
+    </li> -->
+
+<!--     <li class="nav-item active">
+        <a class="nav-link" href="payments.php">
+            <span>Payments</span></a>
+    </li> -->
+
+<!--     <li class="nav-item active">
+        <a class="nav-link" href="feedback.php">
+            <span>Feedback</span></a>
+    </li> -->
+
+<!--     <li class="nav-item active">
+        <a class="nav-link" href="reports.php">
+            <span>Reports</span></a>
+    </li>
+ -->
+
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
